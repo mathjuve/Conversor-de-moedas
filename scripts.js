2dbd60a661a223a4f8a1bcfde57b07e31ab1c018
mathjuve <matheus.98562020@gmail.com>
2022-06-30 19:14:01 -0300

    Meu primeiro commit.

---
+++ scripts.js
@@ -0,0 +1,53 @@
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+
+const dolar = 5.2
+const euro = 5.9
+
+convertValues = () => {
+    const inputReal = document.getElementById('input-real').value
+    const realValueText = document.getElementById('real-value-text')
+    const valueConverted = document.getElementById('value-converted')
+
+    console.log(inputReal / dolar)
+
+    realValueText.innerHTML = Intl.NumberFormat('pt-BR', {
+        style: "currency",
+        currency: "BRL",
+    }
+    ,).format(inputReal)
+
+    if (select.value === "US$ Dólar Americano") {
+        valueConverted.innerHTML = Intl.NumberFormat('en-US'
+            , {
+                style: "currency",
+                currency: "USD",
+            }).format(inputReal / dolar)
+    }
+
+    if (select.value === "€ Euro") {
+        valueConverted.innerHTML = Intl.NumberFormat('de-DE'
+            , {
+                style: "currency",
+                currency: "EUR",
+            }).format(inputReal / euro)
+    }
+}
+
+chanceCurrency = () => {
+
+    const currencyName = document.getElementById('currency-name')
+    const currencyImg = document.getElementById('currency-img')
+
+    if (select.value === '€ Euro') {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./img/logo-euro.png"
+    }
+    if (select.value === 'US$ Dólar Americano') {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImg.src = "./img/logo-eua.png"
+    }
+    convertValues ()
+}
+button.addEventListener('click', convertValues)
+select.addEventListener('change', chanceCurrency)
